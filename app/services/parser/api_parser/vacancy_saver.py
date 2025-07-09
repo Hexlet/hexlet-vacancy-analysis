929db58a68401144b7a45408cac3b81f9808fd3a
Lugovskikh Konstantin <kostya.lugovskikh@mail.ru>
2025-07-09 18:07:59 +0500

    add models and update vacancy_saver.py

---
+++ app/services/parser/api_parser/vacancy_saver.py
@@ -0,0 +1,17 @@
+from parser.models import HhVacancy, SuperjobVacancy
+
+
+class VacancySaver:
+    @staticmethod
+    def save_vacancy(vacancy_data, source='hh'):
+        if source == 'hh':
+            model = HhVacancy
+            id_field = 'hh_id'
+        else:
+            model = SuperjobVacancy
+            id_field = 'superjob_id'
+
+        model.objects.update_or_create(
+            **{id_field: vacancy_data['source_id']},
+            defaults=vacancy_data
+        )
