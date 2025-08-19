b3c4057a99e5766aad21832e38630d2ad3c9c6e3
Lugovskikh Konstantin <kostya.lugovskikh@mail.ru>
2025-08-19 15:05:19 +0500

    fix tests

--- app/services/parser/tests.py
+++ app/services/parser/tests.py
@@ -11,10 +11,10 @@ from http import HTTPStatus
 import os
 
 class VacancyTest(TestCase):
-    fixtures = ['hh_vacancy_detail.json', 'sj_vacancy_detail.json']
+    # fixtures = ['hh_api.json', 'superjob_api.json']
 
     def setUp(self):
-        data = get_fixture_data(os.path.join(FIXTURE_PATH, 'data_dump.json'))
+        data = get_fixture_data(os.path.join(FIXTURE_PATH, 'data_parser.json'))
         self.hh_data = data.get('hh_vacancy')
         self.sj_data = data.get('sj_vacancy')
         self.hh_list = data.get('hh_list_response')
@@ -37,8 +37,8 @@ class VacancyTest(TestCase):
         parser = HhVacancyParser()
         result = parser.parse_vacancy(self.hh_data)
 
-        self.assertEqual(result['id'], "120877600")
-        self.assertEqual(result['name'], "Python Developer")
+        self.assertEqual(result['hh_id'], "120877600")
+        self.assertEqual(result['title'], "Python Developer")
         self.assertEqual(result['salary'], "от 100000 до 200000 RUR")
         self.assertEqual(result['key_skills'], "Python, Django")
 
@@ -48,17 +48,19 @@ class VacancyTest(TestCase):
         parser = SuperjobVacancyParser()
         result = parser.parse_vacancies({})
 
-        self.assertEqual(len(result), 1)
-        self.assertEqual(result[0]['id'], 987654)
-        self.assertEqual(result[0]['name'], "Python разработчик")
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0]['superjob_id'], 987654)
+        self.assertEqual(result[0]['title'], "Python разработчик")
 
     def test_vacancy_saver(self):
         saver = VacancySaver()
         test_data = {
-            'hh_id': 999,
+            'hh_id': '999',
             'title': 'Test Vacancy',
             'company_name': 'Test Company',
-            'url': 'http://test.com'
+            'company_id': '111',
+            'url': 'http://test.com',
+            "published_at": "2023-05-15T12:00:00+0300",
         }
         saver.save_vacancy(test_data, source='hh')
         self.assertTrue(HhVacancy.objects.filter(hh_id=999).exists())
@@ -81,4 +83,3 @@ class VacancyTest(TestCase):
         )
 
         self.assertEqual(response.status_code, HTTPStatus.OK.value)
-        self.assertEqual(response.json()['saved_count'], 1)
