39a94cbc93ea465bc7fd45b63d8870c9d78dc144
Lugovskikh Konstantin <kostya.lugovskikh@mail.ru>
2025-09-25 18:12:24 +0500

    add json.dump in parser.py and add json file

--- app/parser.py
+++ app/parser.py
@@ -5,3 +5,7 @@ def get_fixture_data(path):
     with open(path, 'r') as f:
         data = json.load(f)
         return data
+
+def save_data(path, data):
+    with open(path, 'w') as f:
+        json.dump(data, f)
