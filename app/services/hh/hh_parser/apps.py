449bb0cc36d42f4d2d06e5500c9b27b12ec29c81
Lugovskikh Konstantin <kostya.lugovskikh@mail.ru>
2025-10-24 14:39:32 +0500

    merge with main

--- app/services/hh/hh_parser/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HhParserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app.services.hh.hh_parser'
--- app/services/superjob/superjob_parser/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SuperjobParserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app.services.superjob.superjob_parser'
