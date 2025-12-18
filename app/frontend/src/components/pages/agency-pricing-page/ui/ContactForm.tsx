d0082370e5362adc11456d1dffb8cb36addab454
Maria Bazhenova <yankeevsdixie@gmail.com>
2025-12-18 17:15:11 +0300

    refactor: added phoneRegExp and min-max for company and name

--- app/frontend/src/components/pages/agency-pricing-page/ui/ContactForm.tsx
+++ app/frontend/src/components/pages/agency-pricing-page/ui/ContactForm.tsx
@@ -12,26 +12,38 @@ export interface ContactFormData {
  phone: string;
 }
 
+const phoneRegExp = /^\+7(?:[()\s-]*\d){10}$/;
+
 const contactFormSchema = yup.object().shape({
  companyName: yup
   .string()
+  .min(3, 'Название компании должно содержать не менее 3 символов')
+  .max(100, 'Название компании не должно превышать 100 символов')
   .required('Поле "Название компании" обязательно для заполнения'),
 
- fullName: yup.string().required('Поле "Ваше имя" обязательно для заполнения'),
+ fullName: yup
+   .string()
+   .min(3, 'Ваше имя должно содержать не менее 3 символов')
+   .max(100, 'Ваше имя не должно превышать 100 символов')
+   .required('Поле "Ваше имя" обязательно для заполнения'),
 
  email: yup
   .string()
   .email("Введите корректный Email адрес")
   .required('Поле "Email" обязательно для заполнения'),
 
- phone: yup.string().required('Поле "Телефон" обязательно для заполнения'),
+ phone: yup
+   .string()
+   .required('Поле "Телефон" обязательно для заполнения')
+   .matches(
+      phoneRegExp,
+      'Введите корректный номер телефона, начинающийся с +7'
+   )
 });
 
 // сам компонент
 
 const ContactForm = () => {
-
- // хук useForm
  const {
   register,
   handleSubmit,
@@ -48,7 +60,6 @@ const ContactForm = () => {
  });
 
  const onSubmit: SubmitHandler<ContactFormData> = (data) => {
-  console.log(data);
   reset();
  };
 
@@ -108,7 +119,7 @@ const ContactForm = () => {
       },
      }}
      {...register("email")}
-     error={errors.fullName?.message}
+     error={errors.email?.message}
     />
     <TextInput
      placeholder="Телефон"
@@ -126,7 +137,7 @@ const ContactForm = () => {
       },
      }}
      {...register("phone")}
-     error={errors.fullName?.message}
+     error={errors.phone?.message}
     />
     <Button
      variant="filled"
