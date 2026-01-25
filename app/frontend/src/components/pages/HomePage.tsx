43e8feba4ada526f9503f8d424236729df04312f
Maria Bazhenova <yankeevsdixie@gmail.com>
2026-01-25 20:13:55 +0300

    fix HomePage

--- app/frontend/src/components/pages/HomePage.tsx
+++ app/frontend/src/components/pages/HomePage.tsx
@@ -1,11 +1,8 @@
-import HeroSection from "./home-page/ui/HeroSection";
-
 const HomePage = () => {
   return (
     <div>
-      {/* <h1 className="text-3xl font-bold">Главная</h1>
-      <p className="mt-4">Информация.</p> */}
-      <HeroSection/>
+       <h1 className="text-3xl font-bold">Главная</h1>
+      <p className="mt-4">Информация.</p> 
     </div>
   );
 };
