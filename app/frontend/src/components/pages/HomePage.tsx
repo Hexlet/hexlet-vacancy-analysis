7c15805a8d8e658f5311ece9d6616456b26b9345
Maria Bazhenova <yankeevsdixie@gmail.com>
2026-01-26 13:07:38 +0300

    fix HomePage.tsx

--- app/frontend/src/components/pages/HomePage.tsx
+++ app/frontend/src/components/pages/HomePage.tsx
@@ -1,11 +1,10 @@
-import StatictiscBlock from "./home-page/ui/StatisticsBlock";
-
 const HomePage = () => {
-  return (
-    <div>
-      <StatictiscBlock/>
-    </div>
-  );
+ return (
+  <div>
+   <h1 className="text-3xl font-bold">Главная</h1>
+   <p className="mt-4">Информация.</p>
+  </div>
+ );
 };
 
 export default HomePage;
